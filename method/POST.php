c89ede51ae406ffcbe884e30c5540de3835e7fdb
hace4 <shmelkov03012006@gmail.com>
2023-07-31 00:53:48 +0300

    clear

--- method/POST.php
+++ method/POST.php
@@ -6,9 +6,7 @@ use modules\METHOD_modules\validation;
 
 $params =  validation::validation($_GET) ;
 
-if ( !$params ){
-    
-}else{
+if ( $params ){   
     CRUD::add_data($params['full_name'], $params['organization'], $params['phone_number'], $params['EMAIL'], $params['date'], $params["photo"]);
 }
 
